cdc8f58727541b43ca4db45fabb3018819797701
福知山よんぱちさん <48090196+Yon4800@users.noreply.github.com>
2023-11-30 10:14:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ import paramiko
 # .envファイルの内容を読み込見込む
 load_dotenv()
 
-HOSTNAME = os.environ["SSH"]
-USERNAME = os.environ["RUSERNAME"]
-PASSWORD = os.environ["RPASSWD"]
-LINUX_COMMAND1 = f"echo {os.environ['RPASSWD']} | sudo -S shutdown -h now"
-LINUX_COMMAND2 = f"echo {os.environ['RPASSWD']} | sudo -S reboot"
+#HOSTNAME = os.environ["SSH"]
+#USERNAME = os.environ["RUSERNAME"]
+#PASSWORD = os.environ["RPASSWD"]
+#LINUX_COMMAND1 = f"echo {os.environ['RPASSWD']} | sudo -S shutdown -h now"
+#LINUX_COMMAND2 = f"echo {os.environ['RPASSWD']} | sudo -S reboot"
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -36,44 +36,14 @@ async def on_message(message):
         return
 
     # サーバー起動
-    if message.content.startswith("$サーバーを起動して"):
+    if message.content.startswith("$サーバーは起動している？"):
         # Ping
         p = pings.Ping()
         res = p.ping(os.environ["SSH"])
         if res.is_reached():
             await message.channel.send("サーバーは既に起動しています")
         else:
-            send_magic_packet(os.environ["WOL"])
-            await message.channel.send("サーバーを起動しました")
-
-    # サーバー停止
-    if message.content.startswith("$サーバーを停止して"):
-        with paramiko.SSHClient() as clientp:
-            try:
-                clientp = paramiko.SSHClient()
-                clientp.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                clientp.connect(
-                    hostname=HOSTNAME, port=22, username=USERNAME, password=PASSWORD
-                )
-                stdin, stdout, stderr = clientp.exec_command(LINUX_COMMAND1)
-                await message.channel.send("サーバーを停止しました")
-            except Exception as e:
-                await message.channel.send(f"エラー！: {e}")
-
-    # サーバー再起動
-    if message.content.startswith("$サーバーを再起動して"):
-        with paramiko.SSHClient() as clientp:
-            try:
-                clientp = paramiko.SSHClient()
-                clientp.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                clientp.connect(
-                    hostname=HOSTNAME, port=22, username=USERNAME, password=PASSWORD
-                )
-                stdin, stdout, stderr = clientp.exec_command(LINUX_COMMAND2)
-                await message.channel.send("サーバーを再起動しました")
-            except Exception as e:
-                await message.channel.send(f"エラー！: {e}")
-
+            await message.channel.send("サーバーは閉じています。")
 
 # os.environを用いて環境変数を表示させます
 client.run(os.environ["DISTOKEN"])
